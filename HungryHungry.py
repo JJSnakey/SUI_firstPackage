068b7016461b1804ff3d8ea97a73e13bfcbd104c
Joshua Greer <jjgreer@asu.edu>
2024-03-30 12:57:23 -0400

    set up virtual environment
    starting the py game

---
+++ HungryHungry.py
@@ -0,0 +1,12 @@
+import suiCommunication
+
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the window
+WIDTH, HEIGHT = 800, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sample Game Window")
